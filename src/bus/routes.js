c42d12d783f0aa55fc985be39ca01e7473959d4a
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-10-16 08:50:30 +0800

    missing returns on early returns

--- src/bus/routes.js
+++ src/bus/routes.js
@@ -13,16 +13,20 @@ const wanStop = '25349'
 routes.get('/', async (req, res) => {
   const data = await Client.busStopTimes(wanStop)
   if (data.error) {
-    res.status(500).json({error})
+    console.error('inside if error', data.error)
+    res.status(500).json({ error: data.error })
+    return
   }
   if (data.buses.length <= 0) {
-    res.status(200).json({ message: 'No More buses today'})
+    res.status(200).json({ message: 'No More buses today' })
+    return
   }
   data.buses.forEach(busTime => {
     busTime.timeToLeave = timeToLeave(busTime.time);
   });
-
+  console.log('data log::', data)
   res.status(200).json(data.buses)
+  return;
 })
 
 module.exports = {
