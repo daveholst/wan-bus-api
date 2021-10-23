fbd5e3fca37fe6a39ec806d2b706f63775580d6c
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-10-23 11:19:22 +0800

    updated for endpoint

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const {
 const app = express();
 
 app.use(cors());
-app.use('/bus', busRoutes)
+app.use('/api/bus', busRoutes)
+app.get('/api/', (req, res) => {
+  res.json({ message: 'hit the api!!', reqData: req.body })
+  console.log(req.body)
+})
+
 
 module.exports = app;
